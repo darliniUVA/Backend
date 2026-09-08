c2aad530d3a8d5cc9e25993be1d446ff3cf20644
MP-Alumno <MP-Alumno@duoc.acad>
2026-09-08 17:35:39 -0300

    Initial commit

---
+++ src/main/java/Azure/Backend/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package Azure.Backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+}
---
+++ src/main/java/Azure/Backend/controller/OrdenController.java
@@ -0,0 +1,18 @@
+package Azure.Backend.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/ordenes")
+public class OrdenController {
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('SCOPE_OT.Create')")
+    public ResponseEntity<?> crearOrden() {
+        return ResponseEntity.ok("Orden creada correctamente");
+    }
+
+}
